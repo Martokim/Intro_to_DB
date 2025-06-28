e41e66087eb189910f6e8e883281efea4bfa338d
Marto <martinkimani42@gmail.com>
2025-06-29 01:03:56 +0300

    Fix exception to use mysql.connector.Error

--- MySQLServer.py
+++ MySQLServer.py
@@ -14,7 +14,6 @@ handle open and close of the DB in your script.
 """
 #!/usr/bin/python3
 import mysql.connector
-from mysql.connector import Error
 
 def create_database():
     connection = None
@@ -23,17 +22,17 @@ def create_database():
         connection = mysql.connector.connect(
             host='localhost',
             user='root',
-            password='@Mysql_101'
+            password='your_mysql_password'
         )
 
         if connection.is_connected():
             cursor = connection.cursor()
             cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
             print("Database 'alx_book_store' created successfully!")
-            
-    except Error as e:
+
+    except mysql.connector.Error as e:
         print(f"Error while connecting to MySQL: {e}")
-        
+
     finally:
         if connection is not None and connection.is_connected():
             connection.close()
