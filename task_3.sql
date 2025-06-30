8c2cc379d9feff2830e14e2365cc5e2e6e19dc80
Marto <martinkimani42@gmail.com>
2025-06-30 14:06:21 +0300

    made some changes

--- task_3.sql
+++ task_3.sql
@@ -4,6 +4,6 @@ Write a script that list all the tables in alx_book_store in your MySQL server.
 The name of the file should be task_3.sql
 The database name will be passed as argument of mysql command
 """
-SELECT * FROM information_schema.tables
-WHERE table_schema = 'alx_book_store';
--- The above query retrieves all tables from the 'alx_book_store' database
+USE alx_book_store;
+SHOW TABLES;
+
